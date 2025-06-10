2c894f92c5c9d42338dfe4bbefaebcd7f974bd30
lakla8 <111302715+lakla8@users.noreply.github.com>
2025-06-11 01:38:29 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import '@syncfusion/ej2-buttons/styles/material.css'
+import '@syncfusion/ej2-calendars/styles/material.css'
+import '@syncfusion/ej2-dropdowns/styles/material.css'
+import '@syncfusion/ej2-inputs/styles/material.css'
+import '@syncfusion/ej2-lists/styles/material.css'
+import '@syncfusion/ej2-layouts/styles/material.css'
+import '@syncfusion/ej2-navigations/styles/material.css'
+import '@syncfusion/ej2-popups/styles/material.css'
+import '@syncfusion/ej2-splitbuttons/styles/material.css'
+import '@syncfusion/ej2-grids/styles/material.css'
+import '@syncfusion/ej2-treegrid/styles/material.css'
+import '@syncfusion/ej2-react-gantt/styles/material.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
