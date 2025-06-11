22fa4405a3fe3975fb755e8ac84bc02a5d113c28
Korniliy Zervas <korniliyzervas@Korniliys-Laptop.local>
2025-06-11 09:14:00 +0300

    revert "revert this in future"
    This reverts commit abf7f553fc92137cf06ababdfe388238010abe89.

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1500, // Increase the limit (in kB)
-    rollupOptions: {
-      output: {
-        // You can also manually split chunks, for example:
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0]
-          }
-        }
-      }
-    }
-  },
   base: "/",
 })
